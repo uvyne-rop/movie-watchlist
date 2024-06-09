5cd3424edf4d0888bd943b76dfd971657f990c40
BlairMutisya <tonnyblair09@gmail.com>
2024-06-10 02:02:30 +0300

    Add add_movie function to cli.py for adding new movies to the watchlist

--- lib/cli.py
+++ lib/cli.py
@@ -13,3 +13,9 @@ def cli():
 @click.argument('director')
 @click.argument('genre')
 @click.option('--category', default=None, help="Category ID for the movie")
+
+def add_movie(title, director, genre, category):
+    """Add a new movie to the watchlist."""
+    session = SessionLocal()
+    movie = Movie.create(session, title, director, genre, category)
+    click.echo(f"Movie added: {movie}")
