ae4f3e2dfae148ef6d05da5530dca15af4bfcf29
BlairMutisya <tonnyblair09@gmail.com>
2024-06-10 01:34:51 +0300

    Add __repr__ and create methods to Review model

--- lib/models.py
+++ lib/models.py
@@ -90,3 +90,13 @@ class Review(Base):
     comment = Column(String, nullable=True)
     movie_id = Column(Integer, ForeignKey('movies.id'))
     movie = relationship('Movie', back_populates='reviews')  
+
+    def __repr__(self):
+        return f"<Review(rating={self.rating}, comment={self.comment}, movie_id={self.movie_id})>"
+
+    @classmethod
+    def create(cls, session, movie_id, rating, comment):
+        review = cls(movie_id=movie_id, rating=rating, comment=comment)
+        session.add(review)
+        session.commit()
+        return review
