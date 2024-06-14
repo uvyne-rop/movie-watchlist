b9499f8913b646db72590da469c8af472dbfa65a
uvyne-rop <ropchiri7@gmail.com>
2024-06-14 19:16:51 +0300

    made a movie watchlist backened using python and sqlite and sqlalchemymy

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
+from models import Base, SessionLocal
 from models import Base
 
 class Category(Base):
